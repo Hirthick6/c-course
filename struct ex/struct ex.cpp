6cebf632aaed1d7995c02ccab9fcff57be7ef755
Hirthick6 <105982406+Hirthick6@users.noreply.github.com>
2022-05-21 22:16:28 +0700

    Add files via upload

---
+++ struct ex/struct ex.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+struct summa
+{
+ int nu;
+ char na;
+}a,b,c;
+int  main()
+{
+
+    printf("nu and na ");
+    scanf("%d",&a.nu);
+    scanf("%s",a.na);
+    printf("num and name ");
+    scanf("%d",&b.nu);
+    scanf("%s",b.na);
+    printf("num and name ");
+    scanf("%d",&c.nu);
+    scanf("%s",c.na);
+    printf("\n %d \t %s",a.nu,a.na);
+    printf("\n %d \t %s",b.nu,b.na);
+    printf("\n %d \t %s",c.nu,c.na);
+return 0;
+}
